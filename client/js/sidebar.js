af22645ecb3a83a1773e015eab4291ac1ef2e8d6
markosk29 <markos.kosa29@gmail.com>
2021-04-16 21:09:58 +0300

    update sidebar

--- client/js/sidebar.js
+++ client/js/sidebar.js
@@ -2,16 +2,33 @@ let sidebar = new Vue({
     el: '#sidebar',
     data: {
         notifications: [],
+        unreadNotifications: 0,
         axiosService: null
     },
     created: function(){
         this.axiosService = notifications();
-
         this.axiosService.get().then(response => (this.notifications = response.data));
+    },
+    mounted: function() {
+        this.countUnreadNotifications();
     },
     methods:{
         outputNotifications() {
             this.notifications.forEach(notification => {console.log(notification.text+ " -- " +notification.read)});
+        },
+        meetsConditions() {
+            return (this.unreadNotifications > 0) && (this.unreadNotifications < 100);
+        },
+        countUnreadNotifications() {
+            setTimeout(() => {
+                this.notifications.forEach(
+                    notification => {
+                        if(notification.read === 'false')
+                        {this.unreadNotifications++}
+                    })
+
+                this.$forceUpdate();
+            }, 50)
         }
     }
 })
