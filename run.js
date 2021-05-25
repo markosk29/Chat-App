f8e84d2ee96ac905084d5d656cbdf2e3a58154f0
Malancas Anamaria <61598616+malancas-anamaria-niculina@users.noreply.github.com>
2021-05-25 21:12:00 +0300

    adaugare baza de date

--- run.js
+++ run.js
@@ -3,12 +3,47 @@ let notifications = require('./src/json/notifications.json');
 
 api.get('/', function(request, response) {
     response.json("node.js backend");
+    connection.query("SELECT * FROM accounts",function(error,rows,fields){
+
+    });
 });
 
 api.get('/notifications', function(request, response) {
     response.json(notifications);
 });
 
+//use mysql
+var mysql = require("mysql");
+
+//create connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    //sa modific inainte sa dau push
+    password: "password",
+    database: "test",
+    port: "3306"
+});
+
+connection.query("INSERT INTO chatgroups(group_id, group_name) VALUES (5, 'Chat5')", (error, rows) => {
+    if (error){
+        console.log("inserare nereusita");
+    }else{
+        console.log("inserare reusita");
+        console.log(rows);
+    }
+})
+
+connection.connect((error) => {
+    if (error){
+        console.log("nu merge mysql");
+    }else{
+        console.log("connected");
+    }
+    
+});
+
+//se porneste si server-ul si sockets
 const server = api.listen(3000, function() {
     console.log('CORS-enabled web server is listening on port 3000...');
 });
@@ -23,11 +58,31 @@ io.on('connection', function(socket) {
     console.log(socket.id);
 
     socket.on('SEND_MESSAGE', function(data) {
+        console.log(data);
         io.emit('MESSAGE', data)
     });
 
     socket.on('JOIN', function(data) {
-        io.emit('JOIN_MSG', data)
+        console.log(data);
+        io.emit('JOIN_MSG', data);
+
+        //save message in database
+        /*connection.query("INSERT INTO messages (message_content) VALUES (''' + data +  ''')", (error, result) => {
+            if (error){
+                console.log("inserare nereusita");
+            }else{
+                console.log("inserare reusita");
+            }
+        });*/
     });
+});
+
+connection.query("SELECT * FROM accounts", (error, result, fields) => {
+    if (error){
+        console.log("select fail");
+    }else{
+        console.log("select reusit");
+        console.log(result);
+    }
 });
 
